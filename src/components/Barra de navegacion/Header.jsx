867d64d6fcd7725ae5daee12e6ce7c7adc71454c
sebygalla <sebygalla@hotmail.com>
2023-04-11 12:31:52 -0300

    commits

--- src/components/Barra de navegacion/Header.jsx
+++ src/components/Barra de navegacion/Header.jsx
@@ -2,23 +2,24 @@ import { useState } from "react";
 import logoTipo from "../../assets/image/tomi-04.png";
 import { FaAlignRight, FaKaggle } from "react-icons/fa";
 
+
+
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
     <header
-      className="flex item-center w-full h-[120px] p-4 bg-slate-900/50 justify-between
-    xl:justify-end z-50'">
+      className="flex item-center w-full p-4 bg-slate-900/30 justify-between xl:justify-end z-50'">
       {/* logotipo */}
-      <div className="w-1/5  h-[80px] text-center bg-orange-400">
+      <div className="w-1/5 text-center bg-orange-400">
         <img src={logoTipo} className="w-[80px]" />
       </div>
 
       {/* navegacion */}
       <nav
-        className={`fixed text-white text-sm  uppercase xl:static w-full h-full ${
+        className={`fixed text-white text-sm font-['Poppins']  uppercase xl:static w-full h-full ${
           showMenu ? "left-0" : "-left-full"
-        } top-0 flex-1 flex flex-col xl:flex-row p-8  items-center bg-slate-900 z-50 justify-start  gap-8 transition-all duration-500`}>
+        } top-0 flex-1 flex flex-col xl:flex-row p-8 items-center bg-slate-900 z-50 justify-start xl:justify-end  gap-8 transition-all duration-500`}>
         {/* close */}
         <button
           onClick={() => {
@@ -28,7 +29,7 @@ const Header = () => {
           <FaKaggle />
         </button>
 
-        <a href="#" className="">
+        <a href="#" className=" ">
           inicio
         </a>
 
