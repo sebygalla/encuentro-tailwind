71418ac0bca4b176af43689c73cee2a84fc90ebf
sebygalla <sebygalla@hotmail.com>
2023-04-12 11:32:35 -0300

    menu responsive

--- src/components/Barra de navegacion/Header.jsx
+++ src/components/Barra de navegacion/Header.jsx
@@ -1,57 +1,55 @@
 import { useState } from "react";
 import logoTipo from "../../assets/image/tomi-04.png";
-import { FaAlignRight, FaKaggle } from "react-icons/fa";
-
-
+import logoTipoDos from "../../assets/image/tomi-02.png";
+import { BsJustifyRight, BsX } from "react-icons/bs";
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <header className="flex item-center p-4 bg-slate-900/60 w-full justify-between xl:justify-end z-50'">
+    <header className="item-center z-50' flex w-full justify-between bg-slate-900/80 absolute p-4 xl:justify-end">
       {/* logotipo */}
-      <div className="w-1/5 text-center bg-orange-400">
+      <div className="w-1/5 text-center">
         <img src={logoTipo} className="w-[80px]" />
       </div>
 
       {/* navegacion */}
       <nav
-        className={`fixed text-white text-sm font-['Poppins']  uppercase xl:static w-full h-full ${
+        className={`fixed h-full w-full font-['Poppins'] text-lg  uppercase text-white xl:static xl:text-[12px] ${
           showMenu ? "left-0" : "-left-full"
-        } top-0 flex-1 flex flex-col xl:flex-row p-8 items-center bg-slate-900 z-50 justify-start xl:justify-end  gap-8 transition-all duration-500`}>
-        {/* close */}
-        <button
-          onClick={() => {
-            setShowMenu(!showMenu);
-          }}
-          className="xl:hidden">
-          <FaKaggle />
-        </button>
-
+        } top-0 z-50 flex flex-1 flex-col  items-center justify-center gap-6 bg-slate-900 p-8 transition-all duration-500  xl:flex-row xl:justify-end xl:gap-3`}>
         <a href="#" className=" ">
           inicio
         </a>
+
+        <span className="hidden xl:block">|</span>
 
         <a href="#" className="">
           nosotros
         </a>
 
+        <span className="hidden xl:block">|</span>
+
         <a href="#" className="">
           ministerios
         </a>
 
+        <span className="hidden xl:block">|</span>
+
         <a href="#" className="">
           contacto
         </a>
+
       </nav>
 
-      {/* hamburguesa */}
+      {/* open and close hamburguer */}
       <button
         onClick={() => {
           setShowMenu(!showMenu);
         }}
-        className="text-xl xl:hidden">
-        <FaAlignRight />
+        className="z-50 text-4xl text-white xl:hidden">
+        
+        {showMenu ? <BsX /> : <BsJustifyRight />}
       </button>
     </header>
   );
