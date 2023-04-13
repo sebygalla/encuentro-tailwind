ad21fc7baab5daca5af6602fd696edae1d19b383
sebygalla <sebygalla@hotmail.com>
2023-04-13 09:23:54 -0300

    mision

--- src/components/Barra de navegacion/Header.jsx
+++ src/components/Barra de navegacion/Header.jsx
@@ -11,14 +11,14 @@ const Header = () => {
     <header className="item-center z-50' flex w-full justify-between bg-slate-900/80 absolute p-4 xl:justify-end">
       {/* logotipo */}
       <div className="w-1/5 text-center">
-        <img src={logoTipo} className="w-[80px]" />
+        <img src={logoTipo} className="w-[70px]" />
       </div>
 
       {/* navegacion */}
       <nav
         className={`fixed h-full w-full font-['Poppins'] text-lg  uppercase text-white xl:static xl:text-[12px] ${
           showMenu ? "left-0" : "-left-full"
-        } top-0 z-50 flex flex-1 flex-col  items-center justify-center gap-6 p-8 transition-all duration-500  xl:flex-row xl:justify-end xl:gap-3`}>
+        } top-0 z-50 flex flex-1 flex-col bg-blue-900 xl:bg-transparent  items-center justify-center xl:h-0 gap-6 p-8 transition-all duration-500  xl:flex-row xl:justify-end xl:gap-3`}>
         <a href="#" className=" ">
           inicio
         </a>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import portada from "../../assets/image/portada2.jpg";
+import portada from "../../assets/image/portada.jpg";
 import Titulo from "../Titulo/Titulo";
 
 
@@ -8,9 +8,9 @@ const Hero = () => {
   return (
 
     <section>
-      <img src={portada} className="w-screen h-screen bg-cover object-cover"/>
+      <img src={portada} className="w-full h-full bg-cover object-cover"/>
       <div className="absolute top-20 left-0 right-0 bottom-0 m-auto">
-        <Titulo/>
+        {/* <Titulo/> */}
       </div>    
     </section>
 
