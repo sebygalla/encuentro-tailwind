843783e4f7be957a81ed1a23dc2d45a7eb798d27
sebygalla <sebygalla@hotmail.com>
2023-04-19 12:44:27 -0300

    commits

--- src/components/Titulo/Titulo.jsx
+++ src/components/Titulo/Titulo.jsx
@@ -1,7 +1,7 @@
 const HeroTitle = () => {
   return (
     <section className=" absolute top-[40%] px-8 py-3 text-white md:top-[20%] xl:top-[40%] xl:ml-8">
-      <div className="grid  max-w-4xl grid-cols-1 gap-2 font-['Poppins'] xl:gap-4">
+      <div className="grid  max-w-4xl grid-cols-1 gap-2 font-['Poppins'] xl:gap-4 text-center">
         <h2 className="text-sm font-medium uppercase">Iglesia Cristiana</h2>
       
         <h1 className="text-3xl font-bold xl:text-8xl">
@@ -10,7 +10,7 @@ const HeroTitle = () => {
         <p className="font-light">
           Bienvenidos a nuestra casa! el lugar de tu encuentro con Dios!{" "}
         </p>
-        <button className="text-md w-48 rounded-md px-4 py-2  bg-blue-500">
+        <button className="text-md mx-auto w-48 rounded-md px-4 py-2  bg-blue-500">
           ver más
         </button>
       </div>
