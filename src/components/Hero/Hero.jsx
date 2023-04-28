a57ed1316f7ac23c336005d21a36fc3dbec4934e
sebygalla <sebygalla@hotmail.com>
2023-04-28 10:57:00 -0300

    main portada

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import Portada from "../../assets/image/portada9.png";
 import PortadaMobile from "../../assets/image/main3.png"
 import HeroTitle from "../Titulo/Titulo";
 import Redes from "../Redes/Redes";
+import PortadaMobileDos from "../../assets/image/main4.png"
 
 
 const Hero = () => {
@@ -10,7 +11,7 @@ const Hero = () => {
     <section>
       <img className="w-full md:flex hidden" src="../../src/assets/image/main2.png"></img>
 
-      <img className="md:hidden" src="../../src/assets/image/main3.png"></img>
+      <img className="md:hidden" src="../../src/assets/image/main4.png"></img>
   
       <HeroTitle/>
       <Redes/>
