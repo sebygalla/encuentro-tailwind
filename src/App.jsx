c8b550a94627be056546791fd00112af73e2f6b4
sebygalla <sebygalla@hotmail.com>
2023-05-06 19:20:01 -0300

    responsive testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,46 @@ import Hero from "./components/Hero/Hero";
 import Testimonial from "../src/components/Testimonial/testimonial";
 import Footer from "./components/Footer/Footer";
 import CardActividades from "./components/card/CardImg";
-import TituloWeb from './components/TitulosWeb/tituloWeb'
-
+import TituloWeb from "./components/TitulosWeb/tituloWeb";
+import Nadia from "../src/assets/image/nadia.jpg";
+import Javier from "../src/assets/image/avatarJavier.png";
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Hero/>
-      <TituloWeb title ='Actividades 👨‍👩‍👦‍👦' />
-      <CardActividades/>
-      <Testimonial/>
+    <div className="bg-gray-200">
+      <Header />
+      <Hero />
+      <TituloWeb title="Actividades 👨‍👩‍👦‍👦 " />
+      <CardActividades />
+
+
+      <div className=" bg-gray-900 p-10">
+        
+        <TituloWeb title="Testimonios 💪 " />
+        <div className="mb-10 mt-10 grid gap-5 xl:p-20 md:grid-cols-2 p-2 xl:grid-cols-3">
+          <Testimonial
+            testimonio="Ut efficitur sapien quis ipsum vestibulum eleifend gravida ac odio. In hac habitasse platea dictumst. Pellentesque fringilla, tortor sit amet sodales gravida, sapien lectus dapibus augue, vitae luctus nisl elit sit amet felis. Mauris semper ipsum et lorem sollicitudin venenatis. Nulla facilisi. Cras convallis mauris ut odio pretium, ut volutpat tortor porttitor. In et congue neque."
+            imagen={Nadia}
+            nombre="Nadia Saballa"
+            description="Miembro Iglesia El Encuentro-Tristan Suarez"
+          />
+
+          <Testimonial
+            testimonio="Ut efficitur sapien quis ipsum vestibulum eleifend gravida ac odio. In hac habitasse platea dictumst. Pellentesque fringilla, tortor sit amet sodales gravida, sapien lectus dapibus augue, vitae luctus nisl elit sit amet felis. Mauris semper ipsum et lorem sollicitudin venenatis. Nulla facilisi. Cras convallis mauris ut odio pretium, ut volutpat tortor porttitor. In et congue neque."
+            imagen={Javier}
+            nombre="Javier Corsi"
+            description="Pastor Iglesia El Encuentro-Tristan Suarez"
+          />
+
+          <Testimonial
+            testimonio="Ut efficitur sapien quis ipsum vestibulum eleifend gravida ac odio. In hac habitasse platea dictumst. Pellentesque fringilla, tortor sit amet sodales gravida, sapien lectus dapibus augue, vitae luctus nisl elit sit amet felis. Mauris semper ipsum et lorem sollicitudin venenatis. Nulla facilisi. Cras convallis mauris ut odio pretium, ut volutpat tortor porttitor. In et congue neque."
+            imagen={Javier}
+            nombre="Javier Corsi"
+            description="Pastor Iglesia El Encuentro-Tristan Suarez"
+            className="bg-green-700"
+          />
+        </div>
+      </div>
       <Footer />
     </div>
   );
--- src/components/Testimonial/testimonial.jsx
+++ src/components/Testimonial/testimonial.jsx
@@ -1,76 +1,23 @@
 import React from "react";
-import Javier from "../../assets/image/avatarJavier.png";
-import Nadia from "../../assets/image/nadia.jpg";
 
-import TituloWeb from "../TitulosWeb/tituloWeb";
-const Testimonial = () => {
+const Testimonial = (props) => {
   return (
-    <><TituloWeb title ='Testimonios 💪' />
-    <div className="grid w-full grid-cols-1 items-center gap-8 p-10 md:grid-cols-2 lg:grid-cols-3">
+    
+    <div className="flex flex-col bg-orange-600 gap-6 rounded-xl p-8">
+      <p className="text-white">{props.testimonio}</p>
+      <div className="flex items-center gap-4">
+        <img
+          src={props.imagen}
+          className="h-16 w-16 rounded-full object-cover ring-4 ring-white"
+        />
 
-      {/* Testimonio */}
-      <div className="flex flex-col gap-6 rounded-xl bg-blue-600 p-8 drop-shadow-xl">
-        <p className="text-white">
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
-          quibusdam ducimus libero ad tempora doloribus expedita laborum saepe
-          voluptas perferendis delectus assumenda rerum, culpa aperiam dolorum,
-          obcaecati corrupti aspernatur a..."
-        </p>
-        <div className="flex items-center gap-4">
-          <img
-            src={Javier}
-            className="h-16 w-16 rounded-full object-cover ring-4 ring-white" />
-          <div>
-            <h3 className="font-bold text-indigo-200">Pastor Javier Corsi</h3>
-            <p className="font-medium text-gray-400">
-              Pastor, Iglesia El Encuentro
-            </p>
-          </div>
+        <div>
+          <h3 className="font-bold text-white">{props.nombre}</h3>
+          <p className="font-medium text-gray-900">{props.description}</p>
         </div>
-      </div>
 
-      {/* Testimonio */}
-      <div className="flex flex-col gap-6 rounded-xl bg-gray-300 p-8 drop-shadow-xl">
-        <p className="text-gray-500">
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
-          quibusdam ducimus libero ad tempora doloribus expedita laborum saepe
-          voluptas perferendis delectus assumenda rerum, culpa aperiam dolorum,
-          obcaecati corrupti aspernatur a..."
-        </p>
-        <div className="flex items-center gap-4">
-          <img
-            src={Nadia}
-            className="h-16 w-16 rounded-full object-cover ring-4 ring-white" />
-          <div>
-            <h3 className="font-bold text-indigo-600">Nadia Saballa</h3>
-            <p className="font-medium text-gray-500">
-              Miembro Iglesia El Encuentro
-            </p>
-          </div>
-        </div>
       </div>
-
-      {/* Testimonio */}
-      <div className="flex flex-col gap-6 rounded-xl bg-red-800 p-8 drop-shadow-xl">
-        <p className="text-white">
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
-          quibusdam ducimus libero ad tempora doloribus expedita laborum saepe
-          voluptas perferendis delectus assumenda rerum, culpa aperiam dolorum,
-          obcaecati corrupti aspernatur a..."
-        </p>
-        <div className="flex items-center gap-4">
-          <img
-            src={Nadia}
-            className="h-16 w-16 rounded-full object-cover ring-4 ring-gray-300" />
-          <div>
-            <h3 className="font-bold text-gray-100">Angel Sayago</h3>
-            <p className="font-medium text-gray-300">
-              Miembro y lider Iglesia El Encuentro
-            </p>
-          </div>
-        </div>
-      </div>
-    </div></>
+    </div>
   );
 };
 
