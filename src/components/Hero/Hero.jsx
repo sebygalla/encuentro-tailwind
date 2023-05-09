135f447a7019f2e2ca4a08e40c02ea204ab2dd6c
sebygalla <sebygalla@hotmail.com>
2023-05-09 13:26:12 -0300

    responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import HeroTitle from "../Titulo/Titulo";
 
 const Hero = () => {
   return (
-    <div className="bg-portada z-10 bg-cover max-w-full h-[80vh] md:h-[50vh] xl:h-[100vh] relative bg-no-repeat bg-[left_-30rem_top_2rem] md:bg-center">
+    <div className="bg-portada z-10 bg-cover max-w-full h-[80vh] md:h-[50vh] xl:h-[100vh] relative bg-no-repeat bg-[left_-33rem_top_2rem] md:bg-center">
       <HeroTitle /> 
     </div>
   );
