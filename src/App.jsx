b22b49713d5eb2e04ed6312c598cee867da02d38
sebygalla <sebygalla@hotmail.com>
2023-05-09 23:04:57 -0300

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,56 @@ import TituloWeb from "./components/TitulosWeb/tituloWeb";
 import Nadia from "../src/assets/image/nadia.jpg";
 import Javier from "../src/assets/image/avatarJavier.png";
 import Actividades from "./components/Card/Actividades";
-import Ninos from "../src/assets/image/cards actividades/card6.jpg"
+import Ninos from "../src/assets/image/cards actividades/card7.jpg";
+import Preadolescentes from "../src/assets/image/cards actividades/card8.jpg"
+import Jovenes from "../src/assets/image/cards actividades/card3.jpg"
+import MatriJovenes from "../src/assets/image/cards actividades/card4.jpg"
+import MatriAdultos from "../src/assets/image/cards actividades/card5.jpg"
 
 function App() {
   return (
-    <div className="bg-gray-200 absolute">
+    <div className="absolute bg-gray-200">
       <Header />
       <Hero />
-      <div className="bg-orange-400 m-20">
-    
-        <TituloWeb title="Actividades 👨‍👩‍👦‍👦 " />
 
-        <div className="bg-green-400 p-5">
-        <Actividades  
-              title="Niños"
-              text="The cake at the Sacher hotel is amongst the finest in all the lands es un servicio."  
-              imagen={Ninos}
-              />
+      <TituloWeb title="Actividades 👨‍👩‍👦‍👦 " />
+      <div className="relative mx-auto mt-24 flex flex-col items-center justify-between overflow-hidden container">
+        <div className="mx-auto mb-36 grid gap-10 md:grid-cols-2 xl:grid-cols-3">
+          <Actividades
+            title="Niños"
+            text="The cake at the Sacher hotel is amongst the finest in all the lands es un servicio."
+            imagen={Ninos}
+          />
+
+          <Actividades
+            title="Preadolescentes"
+            text="The cake at the Sacher hotel is amongst the finest in all the lands es un servicio."
+            imagen={Preadolescentes}
+          />
+
+          <Actividades
+            title="Jovenes"
+            text="The cake at the Sacher hotel is amongst the finest in all the lands es un servicio."
+            imagen={Jovenes}
+          />
+
+          <Actividades
+            title="Matrimonios jovenes"
+            text="The cake at the Sacher hotel is amongst the finest in all the lands es un servicio."
+            imagen={MatriJovenes}
+          />
+
+          <Actividades
+            title="Matrimonios adultos"
+            text="The cake at the Sacher hotel is amongst the finest in all the lands es un servicio."
+            imagen={MatriAdultos}
+          />
         </div>
-    
-      
       </div>
-     
 
- 
-
-
-      <div className=" bg-testimonio bg-cover md:bg-container bg-center relative overflow-hidden contrast-125 p-10">
-        
+      <div className=" md:bg-container relative overflow-hidden bg-testimonio bg-cover bg-center p-10 contrast-125">
         <TituloWeb title="Testimonios 💪 " />
-        <div className="mb-10 mt-10 grid gap-5 xl:p-20 md:grid-cols-2 p-2 xl:grid-cols-3">
+        <div className="mb-10 mt-10 grid gap-5 p-2 md:grid-cols-2 xl:grid-cols-3 xl:p-20">
           <Testimonial
             testimonio="Ut efficitur sapien quis ipsum vestibulum eleifend gravida ac odio. In hac habitasse platea dictumst. Pellentesque fringilla, tortor sit amet sodales gravida, sapien lectus dapibus augue, vitae luctus nisl elit sit amet felis. Mauris semper ipsum et lorem sollicitudin venenatis. Nulla facilisi. Cras convallis mauris ut odio pretium, ut volutpat tortor porttitor. In et congue neque."
             imagen={Nadia}
--- src/components/Titulo/Titulo.jsx
+++ src/components/Titulo/Titulo.jsx
@@ -3,12 +3,12 @@ const HeroTitle = () => {
     <section className="w-full px-5 absolute xl:top-[70%] text-white left-[50%] transform -translate-x-1/2 -translate-y-1/2 md:top-[65%] top-[70%]">
       <div className="mx-auto max-w-4xl font-['Poppins'] md:gap-0 xl:gap-4 text-left">
 
-        <h2 className="xl:text-2xl text-lg font-bold uppercase text-blue-900">Iglesia Cristiana <span className="xl:text-4xl text-2xl">⛪</span></h2>
+        <h2 className="xl:text-xl text-lg font-bold uppercase text-blue-900">Iglesia Cristiana <span className="xl:text-4xl text-2xl">⛪</span></h2>
       
-        <h1 className="text-[50px] -mt-3 font-semibold xl:text-[120px] md:text-[60px]">
+        <h1 className="text-[50px] -mt-3 font-semibold xl:text-[80px] md:text-[60px]">
           EL ENCUENTRO
         </h1>
-        <p className="xl:-m-7 -mt-2 font-light text-1xl md:text-xl xl:text-xl xl:tracking-[.6em]">
+        <p className=" -mt-2 font-light text-1xl md:text-xl xl:text-sm xl:tracking-[.9em]">
           Tristán Suárez
         </p>
         <button className="text-md xl:ml-0 md:ml-0  rounded-md md:mt-6 mt-5 xl:mt-14 px-4 py-2 bg-blue-900">
