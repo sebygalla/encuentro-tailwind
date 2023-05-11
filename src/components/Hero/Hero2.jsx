127228b7f39cb66925a5aea266a263da63e84904
sebygalla <sebygalla@hotmail.com>
2023-05-11 12:30:31 -0300

    respo

--- src/components/Hero/Hero2.jsx
+++ src/components/Hero/Hero2.jsx
@@ -8,7 +8,7 @@ const Hero2 = () => {
     //This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
 
     <section className="relative bg-portada bg-fixed w-full h-[100vh] md:w-full md:h-[100vh] bg-no-repeat bg-cover xl:bg-[left_-5rem] md:bg-[left_-5rem_top] bg-center">
-      <div className="absolute inset-0 bg-black/90 sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+      <div className="absolute inset-0 bg-black/70 sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
 
       <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
         <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right"> 
