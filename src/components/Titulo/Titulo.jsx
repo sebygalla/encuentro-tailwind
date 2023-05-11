1e723e3c15deac5d349e0002b626e5f1d6904fc7
sebygalla <sebygalla@hotmail.com>
2023-05-11 12:27:48 -0300

    responsive black

--- src/components/Titulo/Titulo.jsx
+++ src/components/Titulo/Titulo.jsx
@@ -2,7 +2,7 @@ import BotonDos from "../boton/Boton";
 
 const HeroTitle = () => {
   return (
-    <section className="w-full px-5 absolute xl:top-[70%] text-gray-900 left-[50%] xl:left-[50%] transform -translate-x-1/2 -translate-y-1/2 md:top-[60%] top-[55%]">
+    <section className="w-full px-5 absolute xl:top-[70%] text-white left-[50%] xl:left-[50%] transform -translate-x-1/2 -translate-y-1/2 md:top-[60%] top-[55%]">
       <div className="mx-auto max-w-4xl font-['Poppins'] md:gap-0 xl:gap-4 text-center">
 
         <h2 className="xl:text-2xl text-md font-semibold uppercase text-blue-900">Iglesia Cristiana <span className="xl:text-4xl text-2xl">⛪</span></h2>
