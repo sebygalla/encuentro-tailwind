6bc04c137ce616dcdc69678d78d03b9a7cc68149
sebygalla <sebygalla@hotmail.com>
2023-06-05 21:27:40 -0300

    avatars

--- src/components/Avatares/Avatares3.jsx
+++ src/components/Avatares/Avatares3.jsx
@@ -4,55 +4,55 @@ function Avatares3(props) {
       <div className="flex items-center gap-4">
         <img
           src={props.imagenUno}
-          className="z-[10] h-8 w-8 rounded-full object-cover md:ring-4 ring-white md:h-16 md:w-16 ring-2"
+          className="z-[12] h-8 w-8 rounded-full object-cover md:ring-4 ring-white md:h-16 md:w-16 ring-2"
         />
         <img
           src={props.imagenDos}
-          className="z-[9] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[11] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
         <img
           src={props.imagenTres}
-          className="z-[8] h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[10] h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
         <img
           src={props.imagenCuatro}
-          className="z-[7] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[9] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
         <img
           src={props.imagenCinco}
-          className="z-[6] h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[8] h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
         <img
           src={props.imagenSeis}
-          className="z-[5] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[7] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
         <img
           src={props.imagenSiete}
-          className="z-[4] h-8 w-8  rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[6] h-8 w-8  rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
         <img
           src={props.imagenOcho}
-          className="z-[3] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[5] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
 
         <img
           src={props.imagenNueve}
-          className="z-[2] h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[4] h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
 
         <img
           src={props.imagenDiez}
-          className="z-[2] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[3] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
 
         <img
           src={props.imagenOnce}
-          className="z-[1] h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[2] h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
 
         <img
           src={props.imagenDoce}
-          className="z-[0] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
+          className="z-[1] -m-5 h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
 
         <img
@@ -60,6 +60,7 @@ function Avatares3(props) {
           className="z-[0] h-8 w-8 rounded-full object-cover md:ring-4 ring-white contrast-150 md:h-16 md:w-16 ring-2"
         />
       </div>
+
       <div className="my-3 w-full flex flex-col items-center gap-1">
         <h3 className={`text-sm text-center font-semibold text-${props.color}`}>
           {props.nombre}
