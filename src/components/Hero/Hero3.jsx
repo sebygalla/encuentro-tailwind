d25506dd64fba3eced4b9f9fe1fa7d210e272159
sebygalla <sebygalla@hotmail.com>
2023-06-29 18:31:00 -0300

    marqueee

--- src/components/Hero/Hero3.jsx
+++ src/components/Hero/Hero3.jsx
@@ -8,16 +8,16 @@ const Hero3 = () => {
 
       <div className=" mr-9 flex w-[90%] items-center justify-center">
         <div className="z-20">
-          <img className="z-10 ml-4 w-16  md:w-20" src={logoTipo} />
+          <img className="z-10 ml-4 w-24  md:w-20" src={logoTipo} />
         </div>
 
-        <span className="z-10 h-[55px] w-[1px] bg-white"></span>
+        <span className="z-10 h-[58px] w-[2px] bg-white"></span>
 
         <div className="z-10 text-center text-white md:text-center">
-          <h1 className="ml-2 text-[24px] md:text-4xl md:tracking-[5px]">
+          <h1 className="ml-2 font-semibold text-[22px] md:text-4xl md:tracking-[5px]">
             IGLESIA EL ENCUENTRO
           </h1>
-          <h3 className="md:text-md ml-2 text-[11px] tracking-[4.5px] md:tracking-[13px]">
+          <h3 className="md:text-md ml-2 text-[11px] tracking-[4.5px] md:tracking-[13px] font-thin">
             Tristán Suárez - 2023
           </h3>
         </div>
@@ -25,8 +25,7 @@ const Hero3 = () => {
 
       <Redes />
 
-      <marquee className="text-xs text-white font-extralight font-[Poppins]"> El lugar de tu encuentro con Dios!</marquee>
-
+      <marquee className="text-xs text-white font-extralight font-[Poppins]"> El lugar de tu encuentro con Dios! </marquee>
 
     </section>
   );
