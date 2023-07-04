f35060c4fa32f2e0bea6beed3a7ca5985d196a40
sebygalla <sebygalla@hotmail.com>
2023-07-04 19:00:15 -0300

    boton

--- src/components/boton/Boton.jsx
+++ src/components/boton/Boton.jsx
@@ -3,7 +3,7 @@ const BotonDos = (props) => {
    
       <a
         href="#"
-        className="z-30 rounded-[12px] bg-slate-900 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 mt-4">
+        className="z-30 rounded-[12px] bg-slate-900 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none  mt-4">
         {props.text}
       </a>
 
