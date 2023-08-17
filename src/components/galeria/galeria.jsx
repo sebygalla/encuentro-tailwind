9a5e3d8d314a8bf741c771dfea5653f1db9c438c
sebygalla <sebygalla@hotmail.com>
2023-08-16 22:24:09 -0300

    galeria comprimida

--- src/components/galeria/galeria.jsx
+++ src/components/galeria/galeria.jsx
@@ -1,7 +1,23 @@
-import fotoUno from "../../assets/image/galeria/IMG_4428.jpg"
-import fotoDos from "../../assets/image/galeria/IMG_4452.jpg"
-import fotoTres from "../../assets/image/galeria/IMG_4492.jpg"
-import fotoCuatro from "../../assets/image/galeria/IMG_4485.jpg"
+import fotoUno from "../../assets/image/galeria/1.jpg"
+import fotoDos from "../../assets/image/galeria/2.jpg"
+import fotoTres from "../../assets/image/galeria/3.jpg"
+import fotoCuatro from "../../assets/image/galeria/4.jpg"
+import fotoCinco from "../../assets/image/galeria/5.jpg"
+import fotoSeis from "../../assets/image/galeria/6.jpg"
+import fotoSiete from "../../assets/image/galeria/7.jpg"
+import fotoOcho from "../../assets/image/galeria/8.jpg"
+import fotoNueve from "../../assets/image/galeria/9.jpg"
+import fotoDiez from "../../assets/image/galeria/10.jpg"
+import fotoOnce from "../../assets/image/galeria/11.jpg"
+import fotoDoce from "../../assets/image/galeria/12.jpg"
+import fototrece from "../../assets/image/galeria/13.jpg"
+import fotoCatorce from "../../assets/image/galeria/14.jpg"
+import fotoQuince from "../../assets/image/galeria/15.jpg"
+import fotoDieciseis from "../../assets/image/galeria/16.jpg"
+import fotoDiecisiete from "../../assets/image/galeria/17.jpg"
+import fotoDieciocho from "../../assets/image/galeria/18.jpg"
+import fotoDiecinueve from "../../assets/image/galeria/19.jpg"
+import fotoVeinte from "../../assets/image/galeria/20.jpg"
 
 const Galeria = () =>{
     return (
@@ -24,32 +40,32 @@ const Galeria = () =>{
             <img className="h-auto max-w-full rounded-lg" src={fotoCuatro} alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={fotoCinco} alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
-        </div>
-    </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={fotoCatorce} alt=""/>
         </div>
     </div>
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={fotoSiete} alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={fototrece} alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={fotoNueve} alt=""/>
+        </div>
+    </div>
+    <div className="grid gap-4">
+        <div>
+            <img className="h-auto max-w-full rounded-lg" src={fotoDiez} alt=""/>
+        </div>
+        <div>
+            <img className="h-auto max-w-full rounded-lg" src={fotoOnce} alt=""/>
+        </div>
+        <div>
+            <img className="h-auto max-w-full rounded-lg" src={fotoDoce} alt=""/>
         </div>
     </div>
 </div>
