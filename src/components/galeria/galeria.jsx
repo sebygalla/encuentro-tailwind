0809fd6bf01b5fc12f1cfe1f06bf2d63d597524c
sebygalla <sebygalla@hotmail.com>
2023-08-17 17:01:01 -0300

    galeria

--- src/components/galeria/galeria.jsx
+++ src/components/galeria/galeria.jsx
@@ -1,9 +1,9 @@
-import fotoUno from "../../assets/image/galeria/1.jpg"
+// import fotoUno from "../../assets/image/galeria/1.jpg"
 import fotoDos from "../../assets/image/galeria/2.jpg"
 import fotoTres from "../../assets/image/galeria/3.jpg"
 import fotoCuatro from "../../assets/image/galeria/4.jpg"
 import fotoCinco from "../../assets/image/galeria/5.jpg"
-import fotoSeis from "../../assets/image/galeria/6.jpg"
+// import fotoSeis from "../../assets/image/galeria/6.jpg"
 import fotoSiete from "../../assets/image/galeria/7.jpg"
 import fotoOcho from "../../assets/image/galeria/8.jpg"
 import fotoNueve from "../../assets/image/galeria/9.jpg"
@@ -12,12 +12,11 @@ import fotoOnce from "../../assets/image/galeria/11.jpg"
 import fotoDoce from "../../assets/image/galeria/12.jpg"
 import fototrece from "../../assets/image/galeria/13.jpg"
 import fotoCatorce from "../../assets/image/galeria/14.jpg"
-import fotoQuince from "../../assets/image/galeria/15.jpg"
-import fotoDieciseis from "../../assets/image/galeria/16.jpg"
-import fotoDiecisiete from "../../assets/image/galeria/17.jpg"
-import fotoDieciocho from "../../assets/image/galeria/18.jpg"
-import fotoDiecinueve from "../../assets/image/galeria/19.jpg"
-import fotoVeinte from "../../assets/image/galeria/20.jpg"
+// import fotoDieciseis from "../../assets/image/galeria/16.jpg"
+// import fotoDiecisiete from "../../assets/image/galeria/17.jpg"
+// import fotoDieciocho from "../../assets/image/galeria/18.jpg"
+// import fotoDiecinueve from "../../assets/image/galeria/19.jpg"
+// import fotoVeinte from "../../assets/image/galeria/20.jpg"
 
 const Galeria = () =>{
     return (
@@ -26,7 +25,7 @@ const Galeria = () =>{
 <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-lg" src={fotoUno} alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={fotoOcho} alt=""/>
         </div>
         <div>
             <img className="h-auto max-w-full rounded-lg" src={fotoDos} alt=""/>
