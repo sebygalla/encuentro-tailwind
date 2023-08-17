6e46212d1f54d68f1539f4a452aa000aeab1b128
sebygalla <sebygalla@hotmail.com>
2023-08-17 17:50:15 -0300

    galeria 2

--- src/components/galeria/galeria.jsx
+++ src/components/galeria/galeria.jsx
@@ -1,5 +1,6 @@
 import avatarAle from "../../assets/image/alePastora.png";
-import alabanzaSeba from "../../assets/image/galeria1.png";
+import alabanzaUno from "../../assets/image/galeria1.png";
+import alabanzaDos from "../../assets/image/galeria2.jpg";
 import alabanzaCami from "../../assets/image/nosotros.jpg";
 
 const Galeria = () =>{
@@ -9,13 +10,13 @@ const Galeria = () =>{
 <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-lg" src={alabanzaSeba} alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={alabanzaUno} alt=""/>
         </div>
         <div>
             <img className="h-auto max-w-full rounded-lg" src={avatarAle} alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src={alabanzaCami} alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={alabanzaDos} alt=""/>
         </div>
     </div>
     <div className="grid gap-4">
