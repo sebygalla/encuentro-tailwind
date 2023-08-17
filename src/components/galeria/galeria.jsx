432b0489398b77e8f9845291d241f88c7363c0be
sebygalla <sebygalla@hotmail.com>
2023-08-17 17:23:38 -0300

    alabanzaCami

--- src/components/galeria/galeria.jsx
+++ src/components/galeria/galeria.jsx
@@ -1,24 +1,8 @@
-// import fotoUno from "../../assets/image/galeria/1.jpg"
-import fotoDos from "../../assets/image/2.jpg";
-// import fotoTres from "../../assets/image/galeria/3.jpg"
-// import fotoCuatro from "../../assets/image/galeria/4.jpg"
-// import fotoCinco from "../../assets/image/galeria/5.jpg"
-// import fotoSeis from "../../assets/image/galeria/6.jpg"
-// import fotoSiete from "../../assets/image/galeria/7.jpg"
-// import fotoOcho from "../../assets/image/galeria/8.jpg"
-// import fotoNueve from "../../assets/image/galeria/9.jpg"
-// import fotoDiez from "../../assets/image/galeria/10.jpg"
-// import fotoOnce from "../../assets/image/galeria/11.jpg"
-// import fotoDoce from "../../assets/image/galeria/12.jpg"
-// import fototrece from "../../assets/image/galeria/13.jpg"
-// import fotoCatorce from "../../assets/image/galeria/14.jpg"
-// import fotoDieciseis from "../../assets/image/galeria/16.jpg"
-// import fotoDiecisiete from "../../assets/image/galeria/17.jpg"
-// import fotoDieciocho from "../../assets/image/galeria/18.jpg"
-// import fotoDiecinueve from "../../assets/image/galeria/19.jpg"
-// import fotoVeinte from "../../assets/image/galeria/20.jpg"
+
 
 import avatarAle from "../../assets/image/alePastora.png";
+import alabanzaSeba from "../../assets/image/alabanzaSeba.jpg";
+import alabanzaCami from "../../assets/image/alabanzaCami.jpg";
 
 const Galeria = () =>{
     return (
@@ -27,13 +11,13 @@ const Galeria = () =>{
 <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-lg" src={fotoDos} alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={alabanzaSeba} alt=""/>
         </div>
         <div>
             <img className="h-auto max-w-full rounded-lg" src={avatarAle} alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src={avatarAle} alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={alabanzaCami} alt=""/>
         </div>
     </div>
     <div className="grid gap-4">
