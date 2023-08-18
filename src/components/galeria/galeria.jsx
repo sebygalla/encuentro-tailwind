6213bbb3945f4ec8b0ae4cc9daae8e62aed67d99
sebygalla <sebygalla@hotmail.com>
2023-08-18 08:52:31 -0300

    galery

--- src/components/galeria/galeria.jsx
+++ src/components/galeria/galeria.jsx
@@ -7,6 +7,11 @@ import alabanzaCinco from "../../assets/image/galeria5.jpg";
 import alabanzaSeis from "../../assets/image/galeria6.jpg";
 import alabanzaSiete from "../../assets/image/galeria7.jpg";
 import alabanzaOcho from "../../assets/image/galeria8.jpg";
+import alabanzaNueve from "../../assets/image/galeria9.jpg";
+import alabanzaDiez from "../../assets/image/galeria10.jpg";
+import alabanzaOnce from "../../assets/image/galeria11.jpg";
+import alabanzaDoce from "../../assets/image/galeria12.jpg";
+
 
 
 
@@ -39,24 +44,24 @@ const Galeria = () =>{
     </div>
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-lg" src={alabanzaSeis} alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={alabanzaOnce} alt=""/>
         </div>
         <div>
             <img className="h-auto max-w-full rounded-lg" src={alabanzaOcho} alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src={avatarAle} alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={alabanzaNueve} alt=""/>
         </div>
     </div>
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-lg" src={avatarAle} alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={alabanzaDiez} alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src={avatarAle} alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={alabanzaSeis} alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src={avatarAle} alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src={alabanzaDoce} alt=""/>
         </div>
     </div>
 </div>
